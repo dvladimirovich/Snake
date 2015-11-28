db171b5852db4bd17917b0a6083b61c4b5f5f1e0
dvladimirovich <byrmistrov89@gmail.com>
2015-11-29 00:39:17 +0200

    Ошибся при коммите изменений, не подтянулись классы

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class HorizontalLine
+    {
+        private List<Point> points;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char symbol)
+        {
+            points = new List<Point>();
+            for (int i = xLeft; i <= xRight; i++)
+            {
+                Point p = new Point(i, y, symbol);
+                points.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in points)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        private List<Point> points;
+
+        public VerticalLine(int x, int yTop, int yBottom, char symbol)
+        {
+            points = new List<Point>();
+            for (int i = yTop; i <= yBottom; i++)
+            {
+                Point p = new Point(x, i, symbol);
+                points.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in points)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
