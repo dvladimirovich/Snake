a4cef9ceae90110aa4476488641fff77ba16bae4
dvladimirovich <byrmistrov89@gmail.com>
2015-11-28 22:59:28 +0200

    Создали класс Pointи перенесли туда метод Draw()

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public int X;
+        public int Y;
+        public char Symbol;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(Symbol);
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,25 +10,21 @@ namespace Snake
     {
         private static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char symbol1 = '*';
+            Point p1 = new Point();
+            p1.X = 1;
+            p1.Y = 3;
+            p1.Symbol = '*';
 
-            Draw(x1, y1, symbol1);
+            p1.Draw();
 
-            int x2 = 4;
-            int y2 = 5;
-            char symbol2 = '#';
+            Point p2 = new Point();
+            p2.X = 4;
+            p2.Y = 5;
+            p2.Symbol = '#';
 
-            Draw(x2, y2, symbol2);
+            p2.Draw();
 
             Console.ReadKey();
-        }
-
-        private static void Draw(int x, int y, char symbol)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(symbol);
         }
     }
 }
