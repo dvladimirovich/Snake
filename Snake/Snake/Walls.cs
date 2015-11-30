271f592ea145aa05a7477e42c9c413c2cf992a75
dvladimirovich <byrmistrov89@gmail.com>
2015-11-30 23:50:26 +0200

    Добавлен класс Walls для работы с рамкой игры и управлением врезания в них змейки

---
+++ Snake/Snake/Walls.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Walls
+    {
+        private List<Figure> wallList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+
+            // Отрисовка рамки
+            HorizontalLine upLine = new HorizontalLine(0, mapWidth - 2, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, mapWidth - 2, mapHeight - 1, '+');
+            VerticalLine leftLine = new VerticalLine(0, 0, mapHeight - 1, '+');
+            VerticalLine rightLine = new VerticalLine(mapWidth - 2, 0, mapHeight - 1, '+');
+            
+            wallList.Add(upLine);
+            wallList.Add(downLine);
+            wallList.Add(leftLine);
+            wallList.Add(rightLine);
+        }
+
+        internal bool IsHit(Figure figure)
+        {
+            foreach (Figure wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Draw()
+        {
+            foreach (Figure wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+    }
+}
